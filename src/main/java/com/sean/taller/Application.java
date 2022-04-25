c507141e719b3d7aadd852f77b657ef098c63d30
SeanQO <seanquintero3@gmail.com>
2022-04-25 13:57:40 -0500

    added user services and repo, added user types to command line runner

--- src/main/java/com/sean/taller/Application.java
+++ src/main/java/com/sean/taller/Application.java
@@ -2,12 +2,37 @@ package com.sean.taller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.sean.taller.services.imp.UserServiceImp;
+import com.sean.taller.services.intfcs.UserService;
+import com.sean.taller.user.UserEntity;
+import com.sean.taller.user.UserType;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.edu.taller.ortiz.isabella"})	
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext c = SpringApplication.run(Application.class, args);
+		
+		UserService usi = c.getBean(UserServiceImp.class);
+		
+		UserEntity ue1 = new UserEntity();
+		ue1.setId(123456);
+		ue1.setUsername("admin");
+		ue1.setPassword("{noop}admin");
+		ue1.setType(UserType.ADMINISTRATOR);
+		usi.save(ue1);
+		
+		UserEntity ue2 = new UserEntity();
+		ue2.setId(456789);
+		ue2.setUsername("operator");
+		ue2.setPassword("{noop}operator");
+		ue2.setType(UserType.OPERATOR);
+		usi.save(ue2);
+		
 	}
 
 }
--- src/main/java/com/sean/taller/user/UserEntity.java
+++ src/main/java/com/sean/taller/user/UserEntity.java
@@ -2,7 +2,7 @@ package com.sean.taller.user;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
+import javax.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Entity
@@ -10,6 +10,9 @@ import lombok.Data;
 public class UserEntity {
 
 	@Id
+	private long id;
+	
+	@NotBlank
 	private String username;
 	
 	private String password;
