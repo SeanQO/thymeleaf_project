fdf4ea92e4fe5ef134318ff2dd354acd2d22809a
SeanQO <seanquintero3@gmail.com>
2022-05-13 21:49:51 -0500

    Update WorkOrderController.java

--- src/main/java/com/sean/taller/controller/WorkOrderController.java
+++ src/main/java/com/sean/taller/controller/WorkOrderController.java
@@ -1,6 +1,7 @@
 package com.sean.taller.controller;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.sean.taller.model.prod.Workorder;
 import com.sean.taller.services.intfcs.WorkorderService;
 
